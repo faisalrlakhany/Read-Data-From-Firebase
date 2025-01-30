5d6ab0f6f9a31599605c5433eb3a1aa50df225da
faisalrlakhany <lakhanyfaisal37@gmail.com>
2025-01-30 15:04:03 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-    import { collection, addDoc , getDocs } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js"; 
+    import { collection, addDoc , getDocs , Timestamp , query, orderBy , deleteDoc , doc , updateDoc } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js"; 
     import {db} from "./firestore-config.js";
 
 
@@ -11,6 +11,10 @@
     const container = document.querySelector('.container')
 
 
+
+
+    // Todo Add Button
+
     btn.addEventListener('click' , async(event)=>{
 
        event.preventDefault();
@@ -19,46 +23,57 @@
         console.log(textarea.value);
     
         try {
+      
             const docRef = await addDoc(collection(db, "todos"), {
               title:input.value ,
-              descriptioin:textarea.value
+              descriptioin:textarea.value ,
+              date:Timestamp.fromDate(new Date())
             });
             console.log("Document written with ID: ", docRef.id);
-          } catch (e) {
+
+
+            data.push({id: docRef.id, title: input.value, descriptioin: textarea.value});
+            renderData();
+
+
+            } catch (e) {
             console.error("Error adding document: ", e);
-          }
-
-    
-
-          container.innerHTML += ` <div class="box">
-            <p><span>TITLE  : </span> ${input.value}</p>
-            <p><span>Description : </span> ${textarea.value}.</p>
-           </div>
-          `
+            }
 
           input.value = '';
           textarea.value = '';
 
 
     })
+
   
+
+
+    // Get Stored Data From Firestore
+
    async function readData() {
-        const querySnapshot = await getDocs(collection(db, "todos"));
+
+        const q = query(collection(db, "todos"), orderBy("date" , "desc"));
+
+        const querySnapshot = await getDocs(q);
         querySnapshot.forEach((item) => {
         // console.log(`${item.id} => ${JSON.stringify(item.data())}`);
         // data.push(item.id ,item.data())
         data.push({ id:item.id, ...item.data() });
-
-
-        renderData()
-
         });
+
+        renderData();
+
     }
+
     readData();
 
     console.log(data);
     
 
+
+
+    // Show Data on Page
 
     function renderData(){
 
@@ -70,20 +85,84 @@
         container.innerHTML += ` <div class="box">
             <p><span>TITLE  : </span> ${item.title}</p>
             <p><span>Description : </span> ${item.descriptioin}.</p>
+            <div class="bTn">
+              <button class="edit-Btn">Edit</button>
+              <button class="delete-Btn" >Delete</button>
+            </div>
            </div>
           `
 
 
+          const editBtn = document.querySelectorAll('.edit-Btn');
+
+          // For Edit BUTTON
+          editBtn.forEach((editBtns , index) =>{
+  
+            editBtns.addEventListener('click' , async(event)=>{
+  
+              event.preventDefault();
+              console.log('edit called');
+
+              // get Index Of Selected Button
+
+              console.log(data[index]); 
+            
+
+              // Change The Title
+              const updatedTitle = prompt("ENTER NEW TITLE" , data[index].title);
+              
+              // Change The Description
+
+              const updatedDes = prompt("Add new description " , data[index].descriptioin);
 
 
+                  await updateDoc(doc(db, "todos",data[index].id), {
+                    title : updatedTitle ,
+                    descriptioin : updatedDes 
+                  });
 
+                  // Replace The Text From Older To NEw
 
+                  data[index].title = updatedTitle ;
+            
+                  data[index].descriptioin = updatedDes ;
 
+                  // Again Call The function To reload The data
+
+                  renderData();
+
+                  console.log("TITLE UPDATED : " + updatedTitle);
+                  console.log("Description Updated : " + updatedDes);
+                  
+                  
+          });
+          })
+
+          const deleteBtn = document.querySelectorAll('.delete-Btn');
+
+          // For Delete Button
+          deleteBtn.forEach((deleteBtns , index) =>{
+  
+            deleteBtns.addEventListener('click' , async(event)=>{
+  
+              event.preventDefault();
+              console.log('delete called');
+
+          
+            
+                await deleteDoc(doc(db, "todos", data[index].id));
+
+                data.splice(index, 1);
+
+                console.log("Data deleted");
+
+                renderData();
+          
+          })
+          })
+
+  
       })
-
-
-
-
     }
 
       
